7b8e242c8615fa93e7823e42604f874df82a205f
toge <haritoge02@gmail.com>
2022-11-25 15:25:19 +0900

    公開

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-/*
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
@@ -7,19 +6,9 @@
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
 //	header('Location: '.$uri.'/src/hello.php');
-//	header('Location: '.$uri.'..//src/portfolio.php');
+	header('Location: '.$uri.'../src/portfolio.php');
 //	header('Location: '.$uri.'/src/fuji/fujip.php');
 	exit;
-	*/
+	
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-	<meta charset="utf-8">
-	<title>成功</title>
-	</head>
-<body>
-	<p>hello!</p>
-<body>
-</html>
 Something is wrong with the XAMPP installation :-(
